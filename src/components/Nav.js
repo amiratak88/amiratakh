98e37e82f6b51b9f0ad4742fd7da0c31c840df09
Amirata Khodaparast <amiratak88@gmail.com>
2019-03-18 16:20:43 -0400

    Add resume and set up the download link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ class Nav extends Component {
 				<a className="nav__link" href="#skills">technical skills</a>
 				<a className="nav__link" href="#projects">technical projects</a>
 				<a className="nav__link" href="#contact">contact</a>
-				<a className="nav__link nav__link--resume" href="#">
+				<a target="_blank" href="resume-site-v1.pdf" className="nav__link nav__link--resume">
 					<p>resume</p>
 					<DownloadIcon className="nav__icon" />
 				</a>
