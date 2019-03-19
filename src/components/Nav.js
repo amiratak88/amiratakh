125d0fde793ee7d8b1ecd1c3d2748a23865bbe07
Amirata Khodaparast <amiratak88@gmail.com>
2019-03-18 22:16:26 -0400

    make nav bar responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import DownloadIcon from './icons/DownloadIcon'
 
 class Nav extends Component {
+
+	state = {
+		showNav: false
+	}
+
+	closeNav = () => this.setState({ showNav: false })
+	toggleNav = () => this.setState({ showNav: !this.state.showNav })
+
+	getNavClass = () => {
+		return this.state.showNav ? "nav" : "nav nav--hidden"
+	}
+
 	render() {
 		return (
-			<nav className="nav">
-				<a className="nav__link" href="#home">home</a>
-				<a className="nav__link" href="#skills">technical skills</a>
-				<a className="nav__link" href="#projects">technical projects</a>
-				<a className="nav__link" href="#contact">contact</a>
-				<a target="_blank" href="resume-site-v1.pdf" className="nav__link nav__link--resume">
-					<p>resume</p>
-					<DownloadIcon className="nav__icon" />
-				</a>
-			</nav>
+			<Fragment>
+				<nav className="nav-small">
+					<input id="nav-checkbox" className="nav-small__checkbox" type="checkbox"></input>
+					<label onClick={this.toggleNav} className="nav-small__label" htmlFor="nav-checkbox">
+						<div className="nav-small__hamburger">
+							<div>
+							</div>
+						</div>
+					</label>
+				</nav>
+				<nav className={this.getNavClass()}>
+					<a onClick={this.closeNav} className="nav__link" href="#home">home</a>
+					<a onClick={this.closeNav} className="nav__link" href="#skills">technical skills</a>
+					<a onClick={this.closeNav} className="nav__link" href="#projects">technical projects</a>
+					<a onClick={this.closeNav} className="nav__link" href="#contact">contact</a>
+					<a onClick={this.closeNav} target="_blank" href="resume-site-v1.pdf" className="nav__link nav__link--resume">
+						<p>resume</p>
+						<DownloadIcon className="nav__icon" />
+					</a>
+				</nav>
+			</Fragment>
 		)
 	}
 }
