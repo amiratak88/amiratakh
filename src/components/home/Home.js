cd0049bbedafe3c5f90dda72c8bf00ee6d9fd21e
Amirata Khodaparast <amiratak88@gmail.com>
2019-09-12 03:02:46 -0400

    Update home content and adjust styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,8 +6,11 @@ class Home extends Component {
 	render() {
 		return (
 			<section id="home" className="home">
-				<h1 className="home__heading">Welcome to My Site</h1>
-				<p className="home__text">I’m Amirata, a full stack web developer. My knowledge in HTML, CSS, JavaScript, React.js and Ruby on Rails mixed with passion and determination inspires me to build apps that enable positive change in our everyday lives.</p>
+				<h1 className="home__heading">Amirata Khodaparast</h1>
+				<p className="home__text">Hello, thanks for checking out my portfolio</p>
+				<p className="home__text">I’m a full stack web developer based in greater New York City area.</p>
+				<p className="home__text">I'm proficient in technologies like Ruby on Rails, ReactJS and NodeJS.</p>
+				<p className="home__text">I strive to become a better developer everyday by experimenting with new languages and technologies and learning modern techniques and best practices. Currently seeking a full time position in the web and software industry</p>
 				<div className="scroll">
 					<p className="scroll__text">Scroll</p>
 					<div className="scroll__chevron">
